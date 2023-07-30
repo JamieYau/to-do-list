a8f73d3db2f12df4af1af615e57fcf910b170836
JamieYau <jamie.yau2002@me.com>
2023-07-30 02:45:28 +0100

    Add check for removing from empty todo array

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -144,5 +144,45 @@ describe("Project", () => {
       expect(project.todos).not.toContain(todo1);
       expect(project.todos).not.toContain(todo2);
     });
+
+    test("removes the correct todo from the todos array", () => {
+      // Arrange
+      const todo1 = new Todo(
+        "Buy groceries",
+        "Remember to buy milk, eggs, and vegetables.",
+        "2023-08-31",
+        "High"
+      );
+      const todo2 = new Todo(
+        "Do laundry",
+        "Remember to separate the whites and colors.",
+        "2023-08-31",
+        "High"
+      );
+      project.addTodo(todo1);
+      project.addTodo(todo2);
+      const id1 = todo1.id;
+      // Act
+      project.removeTodo(id1);
+      // Assert
+      expect(project.todos.length).toBe(1);
+      expect(project.todos).not.toContain(todo1);
+      expect(project.todos).toContain(todo2);
+    });
+
+    test("throws an error if todo array is empty", () => {
+      // Arrange
+      const todo = new Todo(
+        "Buy groceries",
+        "Remember to buy milk, eggs, and vegetables.",
+        "2023-08-31",
+        "High"
+      );
+      const id = todo.id;
+      // Act & Assert
+      expect(() => {
+        project.removeTodo(id);
+      }).toThrow("Todo not found.");
+    });
   });
 });
