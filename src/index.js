03795a2d5c853884c36809ae6fb50f25a1fbbe4f
JamieYau <jamie.yau2002@me.com>
2023-08-01 17:36:23 +0100

    Add styling to list items

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import "@fortawesome/fontawesome-free/js/fontawesome.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
+import "@fortawesome/fontawesome-free/js/regular.js";
 import "@fortawesome/fontawesome-free/js/brands.js";
 
 console.log("Hi")
